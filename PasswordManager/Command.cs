2d5a48d2835a47f39ad59a9469fd2ec3be83faee
NinoDS <ninodsgaming@gmail.com>
2021-02-25 08:42:32 +0100

    New Command System

---
+++ PasswordManager/Command.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace PasswordManager
+{
+    public class CommandBase
+    {
+        private string _commandId;
+        private string _commandDescription;
+        private string _commandFormat;
+
+        public string CommandId => _commandId;
+
+        public string CommandDescription => _commandDescription;
+
+        public string CommandFormat => _commandFormat;
+
+        public CommandBase(string id, string description, string format)
+        {
+            _commandId = id;
+            _commandDescription = description;
+            _commandFormat = format;
+        }
+    }
+
+    public class Command: CommandBase
+    {
+        private Action command;
+        public Command(string id, string description, string format, Action command) : base(id, description, format)
+        {
+            this.command = command;
+        }
+
+        public void Invoke()
+        {
+            command.Invoke();
+        }
+    }
+}
--- PasswordManager/Program.cs
+++ PasswordManager/Program.cs
@@ -6,38 +6,95 @@ namespace PasswordManager
 {
     class Program
     {
-        private static string asciiChars = "!\"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~";
+        private static string digits = "1234567890";
+        private static string lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
+        private static string upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        private static string specialChars = "~!@#$%^*-_=+[{]}/;:,.?";
+        public static Command GENERATE_PASSWORD;
+        public static List<object> commandList;
+        public static string input;
         
         static void Main(string[] args)
         {
-            Interpreter(Console.ReadLine());
+            input = Console.ReadLine();
+            GENERATE_PASSWORD = new Command("generate_password", "Generates a safe password", "generate_password",
+                () => CreatePassword(10));
+
+            commandList = new List<object>
+            {
+                GENERATE_PASSWORD
+            };
+            
+            HandleInput();
+        }
+
+        public static void HandleInput()
+        {
+            for (int i = 0; i < commandList.Count; i++)
+            {
+                CommandBase commandBase = commandList[i] as CommandBase;
+
+                if (input.Contains(commandBase.CommandId))
+                {
+
+                    if (commandList[i] as Command != null)
+                    {
+                        (commandList[i] as Command).Invoke();
+                    }
+                    
+                }
+            }
         }
 
         static void Interpreter(string text)
         {
-            string[] textArray = text.Split(" ");
-            int arguments = textArray.Length - 2;
-            if (textArray.Length == 0)
-                return;
+            
+        }
 
-            if (textArray[0] == "pm")
+        static void CreatePassword(int passwordLength)
+        {
+            Random rnd = new Random();
+            string password = "";
+            for (int i = 0; i < passwordLength; i++)
             {
-                if (arguments == 1)
+                password += (digits + lowerCaseLetters + upperCaseLetters + specialChars)[rnd.Next((digits + lowerCaseLetters + upperCaseLetters + specialChars).Length)];
+            }
+            Console.WriteLine(password);
+        }
+
+        static void CheckPassword(string password)
+        {
+            int hasUpperCaseLetters = 0, hasLowerCaseLetters = 0, hasNumbers = 0, hasSymbols = 0;
+            string chars = digits + lowerCaseLetters + upperCaseLetters + specialChars;
+            if (password.Length < 8)
+            {
+                Console.WriteLine("Password is unsafe!");
+            }
+
+            foreach (var i in password)
+            {
+                if (digits.Contains(i))
                 {
-                    if (textArray[1] == "password")
-                    {
-                        
-                        Random rnd = new Random();
-                        int passwordLenth = Int32.Parse(textArray[2]);
-                        string password = "";
-                        for (int i = 0; i < passwordLenth; i++)
-                        {
-                            password += asciiChars[rnd.Next(asciiChars.Length)];
-                        }
-                        Console.WriteLine(password);
-                    }
+                    hasNumbers = 1;
+                }
+                else if (lowerCaseLetters.Contains(i))
+                {
+                    hasLowerCaseLetters = 1;
+                }
+                else if (upperCaseLetters.Contains(i))
+                {
+                    hasUpperCaseLetters = 1;
+                }
+                else if (specialChars.Contains(i))
+                {
+                    hasSymbols = 1;
                 }
             }
+
+            Console.WriteLine(hasNumbers + hasLowerCaseLetters + hasUpperCaseLetters + hasSymbols >= 3
+                ? "Password is safe!"
+                : "Password is unsafe!");
         }
     }
+
 }
